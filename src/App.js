4a6a394255c322be6eaf3a57896d7a527d0a42fc
SeonghakIm <tjdgkr140@naver.com>
2021-10-17 16:53:32 +0900

    2.4: Protection with Proptypes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //새로운 component를 넣고 싶을 때는 app안에 통합하여야 함
 
+import PropTypes from "prop-types";
 
 
 
@@ -7,21 +8,38 @@ const foodILike = [
   {
     id:1,
     name : "goguma",
-    image : "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"},
+    image : "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating : 2.5
+  },
   {
     id:2,
     name : "gamza",
-    image : "http://health.chosun.com/site/data/img_dir/2020/05/07/2020050702573_0.jpg"}
+    image : "http://health.chosun.com/site/data/img_dir/2020/05/07/2020050702573_0.jpg",
+    rating : 4.6
+  },
+  {
+    id:3,
+    name : "oksusu",
+    image : "https://dimg.donga.com/wps/NEWS/IMAGE/2021/07/06/107793926.5.jpg",
+    rating : 5.7
+  }
   ]
 
 //component는 uppercase로 시작해야한다
-function Food({name, image}){
+function Food({name, picture, rating}){
   return (<div>
     <h1> I like {name}</h1>
-    <img src={image} alt={name}/>
+    <h3>{rating}/5.0 </h3>
+    <img src={picture} alt={name}/>
   </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 //dist는 object 매우 중요!
 //component는 html을 반환하는 함수
@@ -29,7 +47,11 @@ function App() {
   return (
     <div className="App">
       {foodILike.map(dish =>(
-       <Food key = {dish.id} name={dish.name} picture = {dish.image}/>
+       <Food 
+       key = {dish.id} 
+       name={dish.name} 
+       picture = {dish.image} 
+       rating = {dish.rating}/>
       ))}
     </div>
   );
