98a7f1519bbc3d619717112b28d9caa0b5a0dcf1
SeonghakIm <tjdgkr140@naver.com>
2021-10-24 04:27:02 +0900

    6.1 Building the Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 //새로운 component를 넣고 싶을 때는 app안에 통합하여야 함
 
 import React from "react";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import { HashRouter, Route } from "react-router-dom";
+
 
 function App(){
-  return <span>something</span>;
+  return (
+    <HashRouter>
+      <Route path="/about" component = {About}>
+      </Route>
+      <Route path="/" exact = {true} component = {Home}>
+      </Route>
+    </HashRouter>
+  );
 }
 
 export default App;
